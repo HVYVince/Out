e06b8a7c852aa1df9141142686c4682c7091fbac
Vincent Tournier <vincetournier@msn.com>
2018-05-10 11:46:34 +0200

    start anew, all good, walls, tables, floor and ceiling detected, vertices need cleaning

---
+++ Assets/Scripts/ScannerMaster.cs
@@ -0,0 +1,52 @@
+﻿using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.SpatialMapping;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScannerMaster : MonoBehaviour, IInputClickHandler {//, IInputHandler {
+    private SpatialMappingManager mappingManager;
+    private SurfaceMeshesToPlanes planesMaker;
+
+    public virtual void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("CLICKED INPUT");
+        mappingManager.StopObserver();
+        planesMaker.MakePlanes();
+    }
+
+   /* public void OnInputDown(InputEventData eventData)
+    {
+        Debug.Log("CLICKED DOWN INPUT");
+        return;
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    {
+        Debug.Log("CLICKED UP INPUT");
+        mappingManager.StopObserver();
+        planesMaker.MakePlanes();
+    }*/
+
+    // Use this for initialization
+    void Awake ()
+    {
+        Debug.Log("LAUNCHED");
+        mappingManager = GetComponentInChildren<SpatialMappingManager>();
+        planesMaker = GetComponentInChildren<SurfaceMeshesToPlanes>();
+        //InputManager.Instance.AddGlobalListener(gameObject);
+    }
+
+    void Start()
+    {
+        Debug.Log("START");
+        InputManager.Instance.PushFallbackInputHandler(gameObject);
+        mappingManager.StartObserver();
+    }
+
+        // Update is called once per frame
+    void Update ()
+    {
+        return;
+	}
+}
